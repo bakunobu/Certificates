1a431d75e0d6f6ad8c85f8617b086bd8e0331f30
Sergei Vakunov <bakunobu@gmail.com>
2021-01-20 07:32:21 +0300

    'homework one'

---
+++ pirple_python/hw_1.py
@@ -0,0 +1,42 @@
+"""
+Homework 1
+variables:
+==========
+Artist: str
+My favourite performer
+Genre: str
+The performer's maing genre
+Song: str
+Favourite song by the performer
+DurationInSeconds: int
+Song duration in seconds
+Language: str
+The language of lyrics
+Album: str
+The album the song was listed
+Year: int
+The year the album was released
+RollingStoneGuideScore: float
+The Rolling Stone Album Guide review score 
+"""
+
+# variable section
+Artist = 'The Offspring'
+Genre = 'punk rock'
+Song = 'Walla Walla'
+DurationInSeconds = 177 # 2m57s
+Language = 'English'
+Album = 'Americana'
+Year = 1998
+RollingStoneGuideScore = 3.5 # That's not fair!
+
+
+# printing section
+print(Artist)
+print(Genre)
+print(Song)
+print(DurationInSeconds)
+print(Language)
+print(Album)
+print(Year)
+print(RollingStoneGuideScore )
