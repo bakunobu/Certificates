bb7a3830eec4d886a7560d9bc6b9b23a0563704d
Sergei Vakunov <bakunobu@gmail.com>
2021-01-21 08:54:17 +0300

    'homework 2'

---
+++ pirple_python/hw_2.py
@@ -0,0 +1,21 @@
+Artist = 'The Offspring'
+Genre = 'punk rock'
+Song = 'Walla Walla'
+isSingle = False
+
+
+def artist(my_artist:str=Artist) -> str:
+    return(my_artist)
+
+
+def genre(my_genre:str=Genre) -> str:
+    return(my_genre)
+
+
+def song(my_song:str=Song) -> str:
+    return(my_song)
+
+
+def is_single(single_info:bool=isSingle) -> bool:
+    return(single_info)
+
